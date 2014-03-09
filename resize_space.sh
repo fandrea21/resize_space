4a7ef787480d04c60d9946fecfa9729196d5f791
fandrea21 <hello@andreatv.it>
2014-03-09 11:44:05 +0100

    Update resize_space.sh

--- resize_space.sh
+++ resize_space.sh
@@ -2,21 +2,25 @@
 #Script per ridimensionare la partiziione di un sito
 #indicando la dimensione in Megs (M) o in Giga (G)
 
-echo "Ridimensiona spazio web..."
-echo -n "Indicare sito web: "
-read NOMESITO
-DIMENSIONE=`du -sh /var/path/$NOMESITO.ext3`
-echo "Dimensione attuale: $DIMENSIONE"
-echo -n "Nuova dimensione indicando l'unità di grandezza: "
+#Edit this var for path
+PATH=/var/path/
+
+
+echo "Resize quota"
+echo -n "Add quota name: "
+read QUOTANAME
+DIM=`du -sh $PATH$QUOTANAME.ext3`
+echo "Size: $DIM"
+echo -n "Insert new size,(M)ega or (G)iga: "
 read NEWSIZE
 echo $NEWSIZE
-echo "Procedo con il ridimensionamento di $NOMESITO..."
-echo "Smonto $NOMESITO"
-umount -f /var/path/$NOMESITO.ext3
-e2fsck -f /var/path/$NOMESITO.ext3
-echo "Rdimensionamento..."
-resize2fs -p /var/path/$NOMESITO.ext3 $NEWSIZE
-echo "Monto il sito"
+echo "Processing $QUOTANAME..."
+echo "Umount $QUOTANAME..."
+umount -f $PATH$QUOTANAME.ext3
+e2fsck -f $PATH$QUOTANAME.ext3
+echo "Resize..."
+resize2fs -p $PATH$QUOTANAME.ext3 $NEWSIZE
+echo "Mount"
 mount -a
-echo -n "Rdimensionamento $NOMESITO avvenuto, la nuova dimensione è: "
-du -sh /var/path/$NOMESITO.ext3
+echo -n "That's ok!"
+du -sh $PATH$QUOTANAME.ext3
